f2d9891782de6efa4c5f4bab00b13ccda3b74675
arya <a.hashemi.77@gmail.com>
2025-02-15 14:08:08 +0330

    Add project files.

---
+++ Model/DomainModels/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Model.DomainModels
+{
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Model/DomainModels/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Model.DomainModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
